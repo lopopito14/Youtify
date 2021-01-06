dd7193da950131b7c3c7fbc3706d7fc8a9cac330
Tarik Benchekroun <tarik.benchekroun@mythalesgroup.io>
2021-01-06 14:12:58 +0100

    Updated settings

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,6 @@
 module.exports = {
   root: true,
-  extends: '@react-native-community',
+  extends: ['@react-native-community', 'airbnb-typescript', 'prettier', 'prettier/@typescript-eslint', 'prettier/react'],
   parser: '@typescript-eslint/parser',
   plugins: ['@typescript-eslint'],
 };
