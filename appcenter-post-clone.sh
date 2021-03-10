85372df8d4f489419e7dba7899ca15a8c95741fc
Tarik Benchekroun <tarik.benchekroun@mythalesgroup.io>
2021-03-10 18:37:37 +0100

    Another try...

--- appcenter-post-clone.sh
+++ appcenter-post-clone.sh
@@ -2,7 +2,8 @@
 
 # Create .env file with Youtube and Spotify Client ID
 
-echo "Creating .env file"
+echo 'Creating .env file'
+
 cat > ./.env <<EOL
 YOUTUBE_CLIENT_ID=${YOUTUBE_CLIENT_ID}
 SPOTIFY_CLIENT_ID=${SPOTIFY_CLIENT_ID}
