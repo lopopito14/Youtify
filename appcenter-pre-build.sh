d6b11bb0e71c6c75dff52b9a7f1c8667c1e3cc1c
Tarik Benchekroun <tarik.benchekroun@mythalesgroup.io>
2021-03-10 17:56:49 +0100

    Another try...

--- appcenter-pre-build.sh
+++ appcenter-pre-build.sh
@@ -2,8 +2,8 @@
 
 # Create .env file with Youtube and Spotify Client ID
 
-echo "YOUTUBE_CLIENT_ID=$YOUTUBE_CLIENT_ID" >> .env
-echo "SPOTIFY_CLIENT_ID=$SPOTIFY_CLIENT_ID" >> .env
-
-echo "Youtube client ID => $YOUTUBE_CLIENT_ID"
-echo "Spotify client ID => $SPOTIFY_CLIENT_ID"
+echo "Creating .env file"
+cat > ./.env <<EOL
+YOUTUBE_CLIENT_ID=${YOUTUBE_CLIENT_ID}
+SPOTIFY_CLIENT_ID=${SPOTIFY_CLIENT_ID}
+EOL
