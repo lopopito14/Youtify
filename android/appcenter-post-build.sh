6051a35384e3407de8f3fc822b413f4f9c075544
Tarik Benchekroun <tarik.benchekroun@mythalesgroup.io>
2021-03-11 10:52:44 +0100

    Finalized pre-build script

--- android/appcenter-post-build.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-
-echo 'Android Post build...'
--- appcenter-pre-build.sh
+++ appcenter-pre-build.sh
@@ -1,3 +1,11 @@
 #!/usr/bin/env bash
 
-echo 'Pre build...'
+# Create .env file with Youtube and Spotify Client ID
+
+echo 'Creating .env file'
+echo 'Source directory => ${APPCENTER_SOURCE_DIRECTORY}'
+
+cat > ./.env <<EOL
+YOUTUBE_CLIENT_ID=${YOUTUBE_CLIENT_ID}
+SPOTIFY_CLIENT_ID=${SPOTIFY_CLIENT_ID}
+EOL
